790c0eac236123efca18ea900c25aed909cc635a
SaI <saisantosh.m02@gmail.com>
2023-08-05 20:21:38 +0530

    this is first

---
+++ src/weatherService.js
@@ -0,0 +1,38 @@
+const API_KEY ='6541351d0968430508647af3cd9a5f7d'
+
+const makeIconURL = (iconId) => `https://openweathermap.org/img/wn/${iconId}@2x.png`
+
+
+const getFormattedWeatherData =async (city,units='metric') => {
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+
+
+
+    const data = await fetch(URL)
+    .then((res) => res.json())
+    .then((data) => data);
+
+
+    const {weather,main: {temp, feels_like, temp_min, temp_max,pressure,humidity} ,wind: {speed},
+sys: {country},name,} =data;
+
+const {description ,icon} =weather[0];
+
+return {
+   description,
+   iconURL: makeIconURL(icon),
+   temp,
+   temp_min,
+   temp_max,
+   pressure,
+   humidity,
+   speed,
+   country,
+   name,
+   feels_like,
+};
+
+
+};
+
+export {getFormattedWeatherData} ;
